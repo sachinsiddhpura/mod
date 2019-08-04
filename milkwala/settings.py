20adb71e6096faaa4e84d2226e5042593cc514f6
Sachin Siddhpura <sachinsids007@gmail.com>
2019-08-04 17:23:28 +0530

    Update settings.py

--- milkwala/settings.py
+++ milkwala/settings.py
@@ -115,11 +115,15 @@ USE_L10N = True
 USE_TZ = True
 
 
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
