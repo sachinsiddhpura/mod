8bb365df5d3910ff2aeb60b4cddaf1f9663d603f
Sachin Siddhpura <sachinsids007@gmail.com>
2019-08-04 17:15:52 +0530

    Update settings.py

--- milkwala/settings.py
+++ milkwala/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x$f3dn(vep^v_%cu(sz9u6&xvljz2y=d6f2*%a3+ne1ld7i@+w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
